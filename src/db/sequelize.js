bce45dfe5688c5163cf510fe0b824818c26a093f
VOGLOSSOU <n969601@gmail.com>
2025-06-05 23:36:29 +0100

    good

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -38,10 +38,8 @@ User.hasOne(Reseller, { foreignKey: 'uniqueUserId', sourceKey: 'uniqueUserId', a
 Reseller.belongsTo(User, { foreignKey: 'uniqueUserId', targetKey: 'uniqueUserId', as: 'user' });
 
 // Associations pour les transactions
-Transaction.belongsTo(Admin, { foreignKey: 'sender', targetKey: 'uniqueUserId', as: 'admin' }); // Pour admin-to-reseller
-Transaction.belongsTo(Reseller, { foreignKey: 'sender', targetKey: 'uniqueResellerId', as: 'reseller' }); // Pour reseller-to-user (expéditeur)
-Transaction.belongsTo(Reseller, { foreignKey: 'receiver', targetKey: 'uniqueResellerId', as: 'receiverReseller' }); // Si besoin de retrouver le revendeur destinataire
-Transaction.belongsTo(User, { foreignKey: 'receiver', targetKey: 'uniqueUserId', as: 'receiverUser' }); // Pour reseller-to-user (destinataire)
+Transaction.belongsTo(Admin, { foreignKey: 'sender', targetKey: 'uniqueUserId', as: 'admin' });
+Transaction.belongsTo(Reseller, { foreignKey: 'receiver', targetKey: 'uniqueResellerId', as: 'reseller' }); 
 
 const initDb = async () => {
   try {

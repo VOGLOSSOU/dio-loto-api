38da5b61904f7d5069ad7b97b055a6727f7abd6b
VOGLOSSOU <n969601@gmail.com>
2025-07-11 19:36:07 +0100

    good

--- app.js
+++ app.js
@@ -69,9 +69,11 @@ require("./src/routes/transaction/getAdminToReseller")(app)
 require("./src/routes/transaction/getResellerToUser")(app)
 require("./src/routes/transaction/getUserToUser")(app)
 require("./src/routes/transaction/getRechargeSummary")(app)
-require("./src/routes/transaction/deleteTransaction")(app) 
+require("./src/routes/transaction/deleteTransaction")(app)
 require("./src/routes/transaction/recharge-admin-to-user")(app)
 require("./src/routes/transaction/getAllAdminToUser")(app) // Récupérer les transactions d'un admin vers un utilisateur
+require("./src/routes/transaction/delete-admin-user")(app) // Annuler une recharge admin → user
+require("./src/routes/transaction/delete-admin-reseller")(app) // Annuler une recharge admin → revendeur
 
 // : Games
 require("./src/routes/games/updateGamesStatus")(app)

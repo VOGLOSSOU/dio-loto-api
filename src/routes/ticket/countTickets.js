6fb8e6b92cb7da35d06c76eb62f3c5e68f302333
VOGLOSSOU <n969601@gmail.com>
2025-11-04 05:22:58 +0100

    so good

--- src/routes/ticket/countTickets.js
+++ src/routes/ticket/countTickets.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
   app.get('/api/tickets/counts', async (req, res) => {
     try {
       const total = await Ticket.count();
+      const enPanier = await Ticket.count({ where: { isCart: true } });
       const enAttente = await Ticket.count({ where: { statut: 'en attente', isCart: false } });
       const valides = await Ticket.count({ where: { statut: 'validé', isCart: false } });
       const invalides = await Ticket.count({ where: { statut: 'invalidé', isCart: false } });
@@ -11,6 +12,7 @@ module.exports = (app) => {
 
       res.json({
         total,
+        enPanier,
         enAttente,
         valides,
         invalides,
